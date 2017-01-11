f33ea6e86f00db8822d979af2fc03a4c74059c84
valerijaspasojevic <spasojevic.valerija@gmail.com>
2017-01-11 19:12:17 +0100

    Update najnoviji_v3.ino

--- najnoviji_v3.ino
+++ najnoviji_v3.ino
@@ -1,12 +1,12 @@
 #include <dht11.h>
 dht11 DHT;  
-#define DHT11_PIN 4 // Temperatura
-int flame = A5 ; //Detekcija vatre
-int val = 0 ; //Promenljiva za detekciju vatre
+#define DHT11_PIN 4 
+int flame = A5 ;
+int val = 0 ;
 int value= 0;
-byte data; // Solarna ploca
-int Relay = 8; //Relay
-int solarMax = 80; // Solarna Maksimalna velicina
+byte data; 
+int Relay = 8; 
+int solarMax = 80; 
 
 void setup() { 
   pinMode(Relay, OUTPUT); 
@@ -15,7 +15,7 @@ void setup() {
 } 
 
 void loop() { 
-  int chk; //Temperatura
+  int chk; 
   chk = DHT.read(DHT11_PIN);
   data = analogRead(0); 
  
@@ -29,20 +29,15 @@ void loop() {
  
   val = analogRead (flame) ;
   
-  
-  
-  //Solarna ploca
   Serial.print("Solar panel = ");
   Serial.println(data);
   
-  //Print fire
   if (val == 0) {
      Serial.println("Fire detected!");
   } else {
     Serial.print("");
   }
   
-   //Print temperature
   Serial.print("Humidity = ");
   Serial.print(DHT.humidity, 1);
   Serial.print(", Temp = ");
